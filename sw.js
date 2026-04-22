b92c6620a7054b4b762e692ab0024c18545cd0e2
jwilder6 <jwilder6@gmail.com>
2026-04-21 22:38:38 -0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = "buffylevel-v2";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll([
+      "./",
+      "./index.html",
+      "./icon.png"
+    ]))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE && caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
