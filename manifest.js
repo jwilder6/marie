40ed019aaa3a91980f93740a09b43eacea404d14
jwilder6 <jwilder6@gmail.com>
2026-04-21 22:39:37 -0700

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,15 @@
+{
+  "name": "BuffyLevel",
+  "short_name": "BuffyLevel",
+  "start_url": "./",
+  "display": "standalone",
+  "background_color": "#0b1120",
+  "theme_color": "#0b1120",
+  "icons": [
+    {
+      "src": "icon.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
